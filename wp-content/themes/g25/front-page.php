2d237ac62cde981096e5c2c696db0861a1a2884a
tanomartin <tanomartin05@gmail.com>
2018-10-08 15:46:47 -0300

    Subida de temas G25

---
+++ wp-content/themes/g25/front-page.php
@@ -0,0 +1,54 @@
+<?php get_header(); ?>
+<body>
+    <!-- Masthead -->
+    <header class="masthead-noticias text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-left mx-auto">
+            <h1 class="mb-5">Red Federal</h1>
+            <h4>Enterate lo que pasasen todo el país</h4>
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+
+<!-- seccion noticias -->
+<?php 
+$temp = $wp_query;
+$wp_query= null;
+$wp_query = new WP_Query();
+$wp_query->query('showposts=2'.'&category_name=nota'.'&paged='.$paged);
+?>
+<section style="background-color:#f0f0f0;padding-top:80px;padding-bottom:80px;">
+	<div class="container">
+		<div class="row">	
+		<?php  while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+			<div class="col-lg-6" style="position:relative;margin-bottom:20px;">
+				<div class="card card-nota">
+				    <div class="imagen">
+				    	 <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full', 'title' => 'Feature image']); ?>
+				    </div>
+				    <div style="width:50%;height:auto;float:right">
+				     	<p class="tag"><?php the_category('','nota',''); ?></p>
+				      	<p class="titulo-nota"><b><a href="<?php the_permalink();?>"><?php the_title();?></a></b></p>
+				      	<p class="descripcion-nota"><a href="<?php the_permalink();?>"><?php echo get_post_meta($post->ID, 'bajada', true); ?></a></p>
+				   		<p class="tag"><?php the_tags( '', ', ', '' ); ?></p>
+				   	</div>
+				 </div>
+			</div>
+		 <?php endwhile; ?> 
+		</div>
+		<div class="row">	
+<?php previous_posts_link('&laquo; Anterior') ?>
+<?php next_posts_link('Siguiente &raquo;') ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
+  </body>
+
+</html>
---
+++ wp-content/themes/g25/single.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+  <body>
+
+
+    <!-- Masthead -->
+    <header class="masthead-noticias text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-left mx-auto">
+            <h1 class="mb-5">Red Federal</h1>
+            <h4>Enterate lo que pasasen todo el país</h4>
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+
+<!-- seccion noticias -->
+<?php  if( have_posts() ){
+	the_post();
+	$category_post = get_the_category();
+	$parent_id = get_the_ID(); ?>
+<section style="background-color:#f0f0f0;padding-top:80px;padding-bottom:80px;">
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 offset-md-2 text-center" style="background-color:#fff;padding:80px;padding-bottom:20px;">
+      <?php the_category('','nota',''); ?>
+      <?php echo get_the_date('M d, Y'); ?>
+      <hr>
+      <h1 style="color:#ee4a6d;" class="text-left"><?php the_title();?></h1>
+     
+      <p class="text-left" style="font-size:18px;"><?php echo get_post_meta($post->ID, 'bajada', true); ?></p>
+      <div style="width:30px;height:5px;background-color:#ee4a6d;margin-bottom:60px"></div>
+       <?php the_content(); ?> 
+    </div>
+
+  </div>
+
+  <div class="row">
+    <div class="col-md-8 offset-md-2 text-center" style="background-color:#fff;">
+      <hr>
+      <div class="row">
+        <div class="col-lg-4">
+          <div style="background-color:#629ae6;padding:10px;">
+            <p style="margin-bottom:0px;">Compartir en Facebook</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div style="background-color:#55acee;padding:10px;">
+            <p style="margin-bottom:0px;">Compartir en Twitter</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div style="background-color:#25d366;padding:10px;">
+            <p style="margin-bottom:0px;">Compartir en Whatsapp</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-md-8 offset-md-2 text-center" style="background-color:#fff;">
+      <hr>
+      <div class="row">
+<?php $posttags = get_the_tags();
+		if ($posttags) {
+		  foreach($posttags as $tag) { ?>
+		   <div class="col-lg-3">
+           		<p><a href="<?php echo get_tag_link($tag->term_id); ?>"><?php echo ($tag->name) ?></a></p>
+           </div>
+<?php	  }
+		}  ?>
+      </div>
+    </div>
+  </div>
+
+<?php } ?>
+
+
+  <div class="row">
+    <div class="col-md-8 offset-md-2 text-center" style="background-color:#fff;">
+      <hr>
+
+    <a href="<?php echo SITE_URL;?>"><p> volver a las noticias </p>  </a>   
+
+    </div>
+  </div>
+
+
+
+</div>
+
+</section>
+
+<?php get_footer(); ?>
+  </body>
+
+</html>
