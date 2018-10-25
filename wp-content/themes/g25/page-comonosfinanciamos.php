ff78b5b185ce8b4789bb1182df51ebdaa82e7cfc
tanomartin <tanomartin05@gmail.com>
2018-10-25 14:41:01 -0300

    Acomodamos el texto de la pagina, el que se carga desde el admin

--- wp-content/themes/g25/page-comonosfinanciamos.php
+++ wp-content/themes/g25/page-comonosfinanciamos.php
@@ -3,9 +3,17 @@
 Template Name: Como nos financiamos
 */
 ?>
-<?php get_header(); 
-$content = wpautop( $post->post_content );?>
-<h3>COMOS NOS FINANCIAMOS.php</h3>
-<p><?php echo $content;?></p>
-
+<?php get_header(); ?>
+<header class="masthead text-center">
+	<div class="container">
+		<div class="row" style="background-color:#fff">
+			<h2 class="title-home">COMOS NOS FINANCIAMOS</h2>
+			<p><?php 
+					$content = wpautop( $post->post_content );
+					echo $content;
+				?>
+			</p>
+		</div>
+	</div>
+</header>
 <?php get_footer(); ?>
