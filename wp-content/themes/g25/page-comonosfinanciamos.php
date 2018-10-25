69ce5ec4c139129b652f2fe4a5911a34cb510b4f
tanomartin <tanomartin05@gmail.com>
2018-10-25 14:36:27 -0300

    Se agrega el contenido cargado en la pagina como nos financiamos en el template propio

--- wp-content/themes/g25/page-comonosfinanciamos.php
+++ wp-content/themes/g25/page-comonosfinanciamos.php
@@ -3,6 +3,9 @@
 Template Name: Como nos financiamos
 */
 ?>
-<?php get_header(); ?>
-COMOS NOS FINANCIAMOS.php
+<?php get_header(); 
+$content = wpautop( $post->post_content );?>
+<h3>COMOS NOS FINANCIAMOS.php</h3>
+<p><?php echo $content;?></p>
+
 <?php get_footer(); ?>
