5cc659d42be9eb3b48b0c0ed4cdc52914dd42b00
mzuccotti <zuccottimartin@hotmail.com>
2018-12-05 04:49:53 -0300

    imágenes comunidad
    Imágenes con un alto de 200px y como background para que queden todas
    del mismo tamaño centradas

--- wp-content/themes/g25/page-comunidad.php
+++ wp-content/themes/g25/page-comunidad.php
@@ -26,7 +26,7 @@ Template Name: Comunidad
 $args = array('showposts' => -1, 'post_type' => 'comunidad');
 $wp_query->query($args); ?>
 
-  <div class="container" style="padding-bottom:100px;">
+<div class="container" style="padding-bottom:100px;">
   <div class="row">
 
 <?php if ( have_posts() ) : // Si existen resultados ?>
@@ -36,57 +36,62 @@ $wp_query->query($args); ?>
             $instagram = get_post_meta($post->ID, 'instagram', true) ;
             $linkedin = get_post_meta($post->ID, 'linkedin', true) ;
             $web = get_post_meta($post->ID, 'web', true) ; ?>
-    <div class="col-lg-3 col-md-3 col-xs-12" style="margin-top:20px;">
+  <div class="col-lg-3 col-md-4 col-xs-12" style="margin-top:20px;">
 
     <div class="image-caption featured card">
 
-<?php if (($facebook != "") || ($twitter != "") || ($instagram != "") || ($linkedin != "") || ($web != "")){ ?>
-        <div style="position:absolute;right:5;top:170px;font-size:35px;">
-          <a data-toggle="collapse" href="#social<?php echo $post->ID ?>" role="button" aria-expanded="false" aria-controls="add">
-            <i class="fas fa-plus-circle add-comunidad"></i>
-          </a>
-        </div>
-<?php } ?>
+      <?php if (($facebook != "") || ($twitter != "") || ($instagram != "") || ($linkedin != "") || ($web != "")){ ?>
+              <div style="position:absolute;right:5;top:170px;font-size:35px;">
+                <a data-toggle="collapse" href="#social<?php echo $post->ID ?>" role="button" aria-expanded="false" aria-controls="add">
+                  <i class="fas fa-plus-circle add-comunidad"></i>
+                </a>
+              </div>
+      <?php } ?>
 
-      
-        <?php the_post_thumbnail('medium', ['class' => 'img-responsive responsive--full img-comunidad', 'title' => 'Feature image']); ?>
+      <!--<?php the_post_thumbnail('medium', ['class' => 'img-responsive responsive--full img-comunidad', 'title' => 'Feature image']); ?>!-->
+
+
+
+<?php
+$thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium');
+list($url, $width, $height, $is_intermediate) = $thumbnail;
+?>
+
+<div class="large" style="height:200px;background-image:url(<?php echo $url; ?>);background-repeat:no-repeat;background-position:center center"></div>
+
 
         <div class="caption">
-
           <h2 class="description"><?php the_title(); // El título ?></h2>
           <p class="cargo"><?php echo get_post_meta($post->ID, 'cargo', true) ; ?></p> 
-
           <div class="rrss-comunidad collapse" id="social<?php echo $post->ID ?>" >
-  
-    <?php if (($facebook != "") || ($twitter != "") || ($instagram != "") || ($linkedin != "")) { ?>
-            <hr style="padding-top:0px;margin-top:0px;">
-    <?php } ?>
-    <?php if ($facebook != "") { ?>
-          	<a href="<?php echo $facebook['url']?>" target="_blank"><i class="fab fa-facebook-f fa-2x fa-fw"></i></a>	
-    <?php } ?>
+            <?php if (($facebook != "") || ($twitter != "") || ($instagram != "") || ($linkedin != "")) { ?>
+                    <hr style="padding-top:0px;margin-top:0px;">
+            <?php } ?>
+            <?php if ($facebook != "") { ?>
+                  	<a href="<?php echo $facebook['url']?>" target="_blank"><i class="fab fa-facebook-f fa-2x fa-fw"></i></a>	
+            <?php } ?>
 
-    <?php if ($twitter != "") { ?>
-            <a href="<?php echo $twitter['url']?>" target="_blank"><i class="fab fa-twitter fa-2x fa-fw"></i></a> 
-    <?php } ?>
+            <?php if ($twitter != "") { ?>
+                    <a href="<?php echo $twitter['url']?>" target="_blank"><i class="fab fa-twitter fa-2x fa-fw"></i></a> 
+            <?php } ?>
 
-    <?php if ($instagram != "") { ?>
-            <a href="<?php echo $instagram['url']?>" target="_blank"><i class="fab fa-instagram fa-2x fa-fw"></i></a> 
-    <?php } ?>
+            <?php if ($instagram != "") { ?>
+                    <a href="<?php echo $instagram['url']?>" target="_blank"><i class="fab fa-instagram fa-2x fa-fw"></i></a> 
+            <?php } ?>
 
-    <?php if ($linkedin != "") { ?>
-            <a href="<?php echo $linkedin['url']?>" target="_blank"><i class="fab fa-linkedin-in fa-2x fa-fw"></i></a> 
-    <?php } ?>
+            <?php if ($linkedin != "") { ?>
+                    <a href="<?php echo $linkedin['url']?>" target="_blank"><i class="fab fa-linkedin-in fa-2x fa-fw"></i></a> 
+            <?php } ?>
 
-    <?php if ($web != "") { ?>
-            <hr style="padding-top:0px;margin-top:15px;">
-            <a href="<?php echo $web['url']?>" target="_blank"><p style="text-decoration:underline">Sitio Web</p></a> 
-    <?php } ?>
-
+            <?php if ($web != "") { ?>
+                    <hr style="padding-top:0px;margin-top:15px;">
+                    <a href="<?php echo $web['url']?>" target="_blank"><p style="text-decoration:underline">Sitio Web</p></a> 
+            <?php } ?>
         </div>
-        </div>
+      </div>
+
     </div>  
-
-    </div>
+  </div>
   <?php endwhile; ?>
 <?php endif; ?>
 
