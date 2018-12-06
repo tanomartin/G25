8c588672f18bded19940e2d806f6821a6cb188a9
tanomartin <tanomartin05@gmail.com>
2018-12-06 12:46:10 -0300

    agregado de control de cantidad de caracteres en la busqueda de notas

--- wp-content/themes/g25/searchform.php
+++ wp-content/themes/g25/searchform.php
@@ -1,4 +1,15 @@
-<form class="form-inline my-2 my-lg-0" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" autocomplete="off">
+<script type="text/javascript">
+function control(formulario) {
+	var palabra = formulario.s.value;
+	if (palabra.length < 4) {
+		alert("La palabra a buscar debe contener por lo menos 4 caracteres");
+		return false;
+	}
+	return true;
+}
+</script>
+
+<form class="form-inline my-2 my-lg-0" method="get" id="searchform" onsubmit="return control(this)" action="<?php echo esc_url( home_url( '/' ) ); ?>" autocomplete="off">
 	<input class="form-control mr-sm-2" type="text" class="field" name="s" id="s" placeholder="Buscar" style="text-align: right;">
 	<input style="display: none" type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'g25' ); ?>" />
 </form>
